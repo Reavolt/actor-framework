4bc554b392169ce37563f81a3356e0b6349a6719
Alexey Medvedev <reavolt@outlook.com>
2022-09-14 10:07:44 +0300

    fix: test only socket_size_type unsigned

--- libcaf_io/caf/io/network/native_socket.hpp
+++ libcaf_io/caf/io/network/native_socket.hpp
@@ -14,33 +14,11 @@
 namespace caf::io::network {
 
 // Annoying platform-dependent bootstrapping.
-#ifdef CAF_WINDOWS
 using setsockopt_ptr = const char*;
 using getsockopt_ptr = char*;
 using socket_send_ptr = const char*;
 using socket_recv_ptr = char*;
-using socket_size_type = int;
-#elif defined(CAF_ANDROID)
-  #if defined(__LP64__) || defined(_LP64)
-    using setsockopt_ptr = const void*;
-    using getsockopt_ptr = void*;
-    using socket_send_ptr = const void*;
-    using socket_recv_ptr = void*;
-    using socket_size_type = unsigned;
-  #else
-    using setsockopt_ptr = const void*;
-    using getsockopt_ptr = void*;
-    using socket_send_ptr = const void*;
-    using socket_recv_ptr = void*;
-    using socket_size_type = int;
-  #endif
-#else
-using setsockopt_ptr = const void*;
-using getsockopt_ptr = void*;
-using socket_send_ptr = const void*;
-using socket_recv_ptr = void*;
 using socket_size_type = unsigned;
-#endif
 
 using signed_size_type = std::make_signed<size_t>::type;
 
--- libcaf_net/caf/detail/socket_sys_aliases.hpp
+++ libcaf_net/caf/detail/socket_sys_aliases.hpp
@@ -8,32 +8,11 @@
 
 namespace caf::net {
 
-#ifdef CAF_WINDOWS
 using setsockopt_ptr = const char*;
 using getsockopt_ptr = char*;
 using socket_send_ptr = const char*;
 using socket_recv_ptr = char*;
-using socket_size_type = int;
-#elif defined(CAF_ANDROID)
-  #if defined(__LP64__) || defined(_LP64)
-    using setsockopt_ptr = const void*;
-    using getsockopt_ptr = void*;
-    using socket_send_ptr = const void*;
-    using socket_recv_ptr = void*;
-    using socket_size_type = unsigned;
-  #else
-    using setsockopt_ptr = const void*;
-    using getsockopt_ptr = void*;
-    using socket_send_ptr = const void*;
-    using socket_recv_ptr = void*;
-    using socket_size_type = int;
-  #endif
-#else
-using setsockopt_ptr = const void*;
-using getsockopt_ptr = void*;
-using socket_send_ptr = const void*;
-using socket_recv_ptr = void*;
 using socket_size_type = unsigned;
-#endif // CAF_WINDOWS
+
 
 } // namespace caf::net
